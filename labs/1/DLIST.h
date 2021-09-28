2f7ea9e5e4ae0e69bd87a3844cab21b8b1be8086
wai4uTAKER <71884195+wai4uTAKER@users.noreply.github.com>
2021-09-28 23:22:29 +0300

    Add files via upload

--- labs/1/DLIST.h
+++ labs/1/DLIST.h
@@ -17,12 +17,12 @@ namespace p2 {
 		double getN() const { return n; }
 
 		double chord() const { return n / sqrt(2); }
-		double polar(double angle) const { return (n * cos(angle) * sin(angle)) / (pow(sin(angle), 3) + pow(sin(angle), 3)); }
+		double polar(double angle) const { return (n * cos(angle) * sin(angle)) / (pow(sin(angle), 3) + pow(cos(angle), 3)); }
 		double curvatureR() const { return n / (8 * sqrt(2)); }
 		double curvatureN() const { return n / 2; }
 		double area() const { return n * n / 6; }
-		double highD() const { return n * sqrt(1.5) * 0.448; }
-		double distanceD() const { return n * sqrt(1.5) * 0.577; }
+		double highD() const { 2n /(3*sqrt(2) *0.68125}
+		double distanceD() const { return n / sqrt(6); }
 	};
 
 }
